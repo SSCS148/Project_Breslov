c2cfbcf1a8a176089372a0c4b2c92a990f18f182
SSCS148 <160610047+SSCS148@users.noreply.github.com>
2024-06-23 18:21:26 +0300

    update

--- server/config/database.js
+++ server/config/database.js
@@ -3,10 +3,10 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize('mydatabaseProject', 'myuser', 'sacha1234', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'postgres',
-  port: '5432',
+  port: process.env.DB_PORT,
   logging: false,
 });
 
