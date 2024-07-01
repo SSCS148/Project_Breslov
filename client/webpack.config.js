15fd1a749f975516ffdb5ada04ae0f3c2cfa79c3
SSCS148 <160610047+SSCS148@users.noreply.github.com>
2024-07-01 18:11:31 +0300

    updates

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,65 +1,65 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
-const webpack = require('webpack');
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const Dotenv = require('dotenv-webpack');
+// const webpack = require('webpack');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    hot: true,
-    historyApiFallback: true,
-    port: 8080, // Assurez-vous que c'est le bon port pour votre application
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]', // Simplified name format
-              outputPath: 'assets/', // Ensure images are saved in the assets folder
-            },
-          },
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new Dotenv({
-      path: './.env', // Assurez-vous que le chemin du fichier .env est correct
-    }),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(process.env)
-    }),
-  ],
-};
+// module.exports = {
+//   mode: 'development',
+//   entry: './src/index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'bundle.js',
+//   },
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, 'dist'),
+//     },
+//     hot: true,
+//     historyApiFallback: true,
+//     port: 8080, // Assurez-vous que c'est le bon port pour votre application
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.jsx?$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['@babel/preset-env', '@babel/preset-react'],
+//           },
+//         },
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//       {
+//         test: /\.(png|jpe?g|gif)$/i,
+//         use: [
+//           {
+//             loader: 'file-loader',
+//             options: {
+//               name: '[name].[ext]', // Simplified name format
+//               outputPath: 'assets/', // Ensure images are saved in the assets folder
+//             },
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: ['.js', '.jsx'],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './src/index.html',
+//     }),
+//     new Dotenv({
+//       path: './.env', // Assurez-vous que le chemin du fichier .env est correct
+//     }),
+//     new webpack.DefinePlugin({
+//       'process.env': JSON.stringify(process.env)
+//     }),
+//   ],
+// };
