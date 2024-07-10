1d967c214ad38ed9ad1218f9ea42789399554d6b
SSCS148 <160610047+SSCS148@users.noreply.github.com>
2024-07-10 14:42:10 +0300

    //

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(cors({
     credentials: true // This is important to allow credentials in CORS
 }));
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
