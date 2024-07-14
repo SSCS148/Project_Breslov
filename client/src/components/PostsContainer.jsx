fa3ba3b50a6053945c900dce2e3793ef67f75be6
SSCS148 <160610047+SSCS148@users.noreply.github.com>
2024-07-14 13:45:35 +0300

    postscontainer and css

--- client/src/components/PostsContainer.jsx
+++ client/src/components/PostsContainer.jsx
@@ -44,7 +44,6 @@ const PostsContainer = () => {
 
     return (
         <div className="posts-container">
-            <PostForm onPostCreated={handlePostCreated} />
             {posts.map(post => (
                 <div key={post.id} className="post">
                     <p>{post.content}</p>
